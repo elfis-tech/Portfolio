7e69353f0bfa58ddc9a95266627bfc19757da5d9
Valentin <valentin-fouilloud.com>
2025-05-07 16:01:06 +0200

    Presque finit BIG BIG BIG milestone

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+const typed= new Typed('.multiple',{
+    strings:["Développeur web","Web Designer","Back-end Développeur","Développeur en Domotique"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
