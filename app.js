8ec5153326d5089cf68b496d44b263ae399c6aa8
Valentin <valentin-fouilloud.com>
2025-05-12 10:57:40 +0200

    FINISH

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
+const sections = document.querySelectorAll('section');
+const linksNav = document.querySelectorAll('navigation a');
+const header = document.querySelector('header');
+const btnHome = document.querySelector('.btn-home');
+const menuIcon = document.querySelector('#menu-burger');
+const nav = document.querySelector('.navigation');
 
+const burgerActive=()=>{
+    menuIcon.classList.toggle('bx-x');
+    nav.classList.toggle('active')
+}
+
+const scrollActive = ()=>{
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset=section.offsetTop -150;
+        let height= section.offsetHeight;
+        let id=section.getAttribute('id')
+        if(top >= offset && top < offset + height){
+            linksNav.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('.navigation a[href*=${id}]').classList.add('active');
+            })
+        }
+    } )
+}
+
+
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration: 2000,
+    delay: 125
+});
+ScrollReveal().reveal('.home-content, .section-title',{origin:'top'})
+ScrollReveal().reveal('.home-img, .services-content, .portfolio-box, .contact-form',{origin:'bottom'})
+ScrollReveal().reveal('.home-content h1, .about-img',{origin:'left'})
+ScrollReveal().reveal('.home-content p, .about-content',{origin:'right'})
 
 const typed= new Typed('.multiple',{
     strings:["Développeur web","Web Designer","Back-end Développeur","Développeur en Domotique"],
@@ -7,3 +44,6 @@ const typed= new Typed('.multiple',{
     backDelay:1000,
     loop:true
 })
+
+menuIcon.addEventListener('click',burgerActive);
+window.addEventListener('scroll',scrollActive);
